ebceed3d6f70c7f25b2023ac9826d8bda558490b
PawKorchagin <146348445+PawKorchagin@users.noreply.github.com>
2024-10-03 19:28:27 +0300

    initial commit

---
+++ L2/target/debug/deps/L2-69a7a1fbc2d26e17.d
@@ -0,0 +1,5 @@
+/Users/pawkorchargin/L2/L2/target/debug/deps/libL2-69a7a1fbc2d26e17.rmeta: src/main.rs
+
+/Users/pawkorchargin/L2/L2/target/debug/deps/L2-69a7a1fbc2d26e17.d: src/main.rs
+
+src/main.rs:
---
+++ L2/target/debug/deps/L2-913ec3f8bf58077a.d
@@ -0,0 +1,5 @@
+/Users/pawkorchargin/L2/L2/target/debug/deps/libL2-913ec3f8bf58077a.rmeta: src/main.rs
+
+/Users/pawkorchargin/L2/L2/target/debug/deps/L2-913ec3f8bf58077a.d: src/main.rs
+
+src/main.rs:
